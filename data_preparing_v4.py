72f3504c9f3c6549ba2586988ca8fa6c31890b56
ting <tingtingchang95@gmail.com>
2017-03-18 22:20:55 +0000

    update to github, it is working

--- data_preparing_v4.py
+++ data_preparing_v4.py
@@ -1,21 +1,25 @@
 import pandas as pd
 import numpy as np
 
-from google.cloud import storage
-from StringIO import StringIO
+# from google.cloud import storage
+# from StringIO import StringIO
 from PIL import Image
 from sklearn import preprocessing
 import sys
 
 
-PROJECT_NAME = 'painting-classifier'
-BUCKET_NAME = 'painting-classifier-data'
+# PROJECT_NAME = 'painting-classifier'
+# BUCKET_NAME = 'painting-classifier-data'
 
-bucket = storage.Client(project = PROJECT_NAME).bucket(BUCKET_NAME)
+# bucket = storage.Client(project = PROJECT_NAME).bucket(BUCKET_NAME)
 
+
+# def get_image_data(image_path):
+# 	im = Image.open(StringIO(bucket.blob(image_path).download_as_string()))
+# 	return np.array(im.getdata(), dtype = np.float32).reshape(1, im.height, im.width, -1) + np.zeros((1, 1, 1, 3))
 
 def get_image_data(image_path):
-	im = Image.open(StringIO(bucket.blob(image_path).download_as_string()))
+	im = Image.open(image_path)
 	return np.array(im.getdata(), dtype = np.float32).reshape(1, im.height, im.width, -1) + np.zeros((1, 1, 1, 3))
 
 
@@ -28,7 +32,7 @@ def get_resized_img_colors(file_list, image_dir):
 	processed = 0
 	for img in file_list:
 		try:
-			img_pixels = get_image_data('resized_200/' + img)
+			img_pixels = get_image_data(image_dir + img)
 			images.append(img_pixels)
 			processed += 1
 			sys.stdout.write(format_str % (processed, total_imgs))
@@ -54,24 +58,29 @@ def get_net_data(painters = None):
 		if isinstance(painters, list):
 			df_data = df_data[df_data['short_name'].isin(painters)]
 
-	# get dummies for all the painters
-	y = pd.get_dummies(df_data.short_name)
-	dummies_cols = y.columns
-	y = np.array(y, dtype = np.float32)
-
-
 	# get images' width & height
 	sizes_paintings = pd.read_csv('./data/images_sizes_2325.csv')
-	sizes_paintings = pd.merge(df_data[['file_name']], sizes_paintings, how = 'left', on='file_name')
-	images_stats = np.array(sizes_paintings[['width', 'height']], dtype = np.float32)
+	width = sizes_paintings.width
+	height = sizes_paintings.height
+
+	ratio_filter = np.logical_and(width / height <= 2, height / width <= 2)
+	sizes_paintings = sizes_paintings[ratio_filter]
+
+	df_all = pd.merge(df_data, sizes_paintings, how = 'inner', on='file_name')
+	images_stats = np.array(df_all[['width', 'height']], dtype = np.float32)
+
+	# get dummies for all the painters
+	y = pd.get_dummies(df_all.short_name)
+	dummies_cols = y.columns
+	y = np.array(y, dtype = np.float32)
 
 	# balance image data: width, height
 	scaler = preprocessing.StandardScaler().fit(images_stats)
 	images_stats = scaler.transform(images_stats)
 
-	images_colors = get_resized_img_colors(df_data['file_name'], 'data/resized_200/')
+	images_colors = get_resized_img_colors(df_all['file_name'], 'data/resized_200/')
 
 
 	return {'images_colors': images_colors, 'images_stats': images_stats, 
-					'response': y, 'painters': dummies_cols, 'indices': df_data[['short_name', 'file_name']]}
+					'response': y, 'painters': dummies_cols, 'indices': df_all[['short_name', 'file_name']]}
 
--- neural_net_v4.py
+++ neural_net_v4.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 import re, sys, time, os
 from keras.models import Sequential
-from keras.layers import InputLayer, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten, Merge
+from keras.layers import InputLayer, Conv2D, MaxPooling2D, Dense, Dropout, Flatten, Merge
 from keras.optimizers import Adadelta
 import tensorflow as tf
 
@@ -13,21 +13,21 @@ def neural_net_images(initial_rate=0.04, output_classes = 3,input_width=200, inp
 	# image
 	image = Sequential()
 	image.add(InputLayer((input_width, input_height, 3), name = 'images_colors'))  # 3*200*200
-	image.add(Convolution2D(32, 5, 5, activation = 'relu')) # 32*196*196
+	image.add(Conv2D(32, (5, 5), activation = 'relu')) # 32*196*196
 	image.add(MaxPooling2D(pool_size = (2, 2))) #32*98*98
-	image.add(Convolution2D(64, 5, 5, activation = 'relu')) # 64*94*94
+	image.add(Conv2D(64, (5, 5), activation = 'relu')) # 64*94*94
 	image.add(MaxPooling2D(pool_size=(2, 2))) # 64*47*47
 	image.add(Dropout(0.2))
-	image.add(Convolution2D(64, 4, 4, activation = 'relu')) # 64*44*44
+	image.add(Conv2D(64, (4, 4), activation = 'relu')) # 64*44*44
 	image.add(MaxPooling2D(pool_size = (2, 2))) #64*22*22
 	image.add(Dropout(0.3))
-	image.add(Flatten())
+	image.add(Flatten()) #30976
 
 	img_size = Sequential()
 	img_size.add(InputLayer((2, ), name = 'images_stats'))
 	
 	img_merged = Sequential()
-	img_merged.add(Merge([image, img_size], mode = 'concat')) #6043
+	img_merged.add(Merge([image, img_size], mode = 'concat')) #30978
 	img_merged.add(Dense(1000, activation = 'relu')) # 1000
 	img_merged.add(Dropout(0.5))
 	img_merged.add(Dense(200, activation = 'relu')) # 200
@@ -40,7 +40,7 @@ def neural_net_images(initial_rate=0.04, output_classes = 3,input_width=200, inp
 
 
 def train_network(data):
-
+	data = data.copy()
 	y = data['response']
 	painters = data['painters']
 	del data['response']
@@ -50,10 +50,10 @@ def train_network(data):
 	net = neural_net_images(output_classes = len(painters))
 	history = net.fit(data, y, validation_split = 0.2)
 	
-	net_predicted = net.predict_classes({'images_colors': images_colors, 'images_stats': images_stats})
-	net_evaluate = net.evaluate({'images_colors': images_colors, 'images_stats': images_stats}, y)
+	net_predicted = net.predict_classes(data)
+	net_evaluate = net.evaluate(data, y)
 
 	pd.concat([indices.reset_index(drop = True), 
 		pd.DataFrame({'prediction': [painters[x] for x in net_predicted]})], axis = 1).to_csv('./data/net_predicted.csv', index = False)
-	
+
 	return net, history, net_evaluate
