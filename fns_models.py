8efadc7286488785ea470b83b37851951e21979b
tingtingchang <tingtingchang95@gmail.com>
2017-03-27 09:56:05 -0400

    update prepare data

--- fns_models.py
+++ fns_models.py
@@ -14,9 +14,9 @@ def get_top_author(num_author=3):
     print "[INFO] The size of test histogram for Random Forest" + str(test.shape)
 
      
-    train.iloc[:,2:-2] = train.iloc[:, 2:-2]\
+    train.iloc[:,2:-3] = train.iloc[:, 2:-3]\
         .apply(lambda x: x.astype(np.float) / (x.sum()/3), axis = 1, raw = True)
-    test.iloc[:,2:-2] = test.iloc[:, 2:-2]\
+    test.iloc[:,2:-3] = test.iloc[:, 2:-3]\
         .apply(lambda x: x.astype(np.float) / (x.sum()/3), axis = 1, raw = True)
         
     author_index = train.author_id.value_counts().index[:num_author]
@@ -32,9 +32,9 @@ def get_top_author(num_author=3):
     train_labels = train.author_id
     print train_labels.shape
     print  train.shape
-    train = train.drop(['author_id', 'painting_id'], axis=1)
+    train = train.drop(['author_id', 'painting_id', 'height_px', 'width_px'], axis=1)
     test_labels = test.author_id
-    test = test.drop(['author_id', 'painting_id'], axis=1)
+    test = test.drop(['author_id', 'painting_id', 'height_px', 'width_px'], axis=1)
    
     return train, train_labels, test, test_labels
 
@@ -51,7 +51,20 @@ def result_table(y_true, y_pred):
     return test_data_df
 
 
+def plot_bo(f, bo):
+    xs = [x["x"] for x in bo.res["all"]["params"]]
+    ys = bo.res["all"]["values"]
 
+    mean, sigma = bo.gp.predict(np.arange(len(f)).reshape(-1, 1), return_std=True)
+    
+    plt.figure(figsize=(16, 9))
+    plt.plot(f)
+    plt.plot(np.arange(len(f)), mean)
+    plt.fill_between(np.arange(len(f)), mean+sigma, mean-sigma, alpha=0.1)
+    plt.scatter(bo.X.flatten(), bo.Y, c="red", s=50, zorder=10)
+    plt.xlim(0, len(f))
+    plt.ylim(f.min()-0.1*(f.max()-f.min()), f.max()+0.1*(f.max()-f.min()))
+    plt.show()
 
 
 
