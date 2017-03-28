3f3b71b85b41e05ae7ccc2ce0ff7c61fb9ac121c
Domingos Lopes <domingos86lopes+github@gmail.com>
2017-03-28 14:43:30 -0400

    handle with pool executor and pickle issues

--- src/loading_utils.py
+++ src/loading_utils.py
@@ -21,6 +21,9 @@ MIN_DIMS = 64
 def get_image_path(author_id, painting_id, thumb = 'full'):
     return os.path.join(IMAGES_PATH, thumb, str(author_id), str(painting_id) + '.jpg')
 
+def get_image_array_unpack(args):
+    return get_image_array(*args)
+
 def get_image_array(author_id, painting_id, thumb = 'full'):
     image_path = get_image_path(author_id, painting_id, thumb)
     im = Image.open(image_path)
@@ -31,7 +34,15 @@ def get_image_array(author_id, painting_id, thumb = 'full'):
             ratio = float(MIN_DIMS) / min(im.width, im.height)
             im = im.resize((int(im.width * ratio), int(im.height * ratio)), Image.LANCZOS)
     return ref, np.array(im.getdata(), dtype = np.float32).reshape(1, im.width, im.height, NUM_CHANNELS) / 255
-	
+
+class RandomCrop(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def __call__(self, np_image):
+        return random_crop(np_image, self.width, self.height)
+
 def random_crop(np_image, width, height):
     x_offset = np.random.randint(np_image.shape[1] - width + 1)
     y_offset = np.random.randint(np_image.shape[2] - height + 1)
@@ -39,7 +50,7 @@ def random_crop(np_image, width, height):
 
 def get_image_crops_batch(list_ids, dict_sizes):
     pool = multiprocessing.Pool(processes=cpus)
-    full_sized = pool.map(lambda id_: get_image_array(*id_), list_ids)
+    full_sized = pool.map(get_image_array_unpack, list_ids)
     result = {}
     for size_name, size in dict_sizes.items():
         width = size[0]
@@ -48,7 +59,8 @@ def get_image_crops_batch(list_ids, dict_sizes):
         height = size[1]
         if height is None:
             height = min([image.shape[2] for image in full_sized])
-        result[size_name] = np.vstack(pool.map(lambda image: random_crop(image, width, height), full_sized))
+        rCrop = RandomCrop(width, height)
+        result[size_name] = np.vstack(pool.map(rCrop, full_sized))
     return result
 
 #def get_image_crop_batch_from_df(df, dict_sizes):
